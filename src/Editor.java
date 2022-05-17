569eaffb10251cb0ab14222fd32e247ba3be87d6
Geraldo Vedrossi Neto <73123156+geraldovedrossi@users.noreply.github.com>
2022-05-17 17:26:09 -0300

    Dia 2 - Aulas 3 e 4

---
+++ src/Editor.java
@@ -0,0 +1,8 @@
+
+public class Editor extends Funcionario{
+	
+	public double getBonificacao() {
+		return super.getBonificacao() + 100;
+	}
+	
+}
--- src/TesteFuncionario.java
+++ src/TesteFuncionario.java
@@ -4,16 +4,24 @@ public class TesteFuncionario {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Gerente nico = new Gerente();
+		Funcionario ge = new Funcionario();
+		Editor ed = new Editor();
 		
-		nico.setNome("Nico Steppat");
-		nico.setCpf("1114");
-		nico.setSalario(3000);
-		nico.setSenha(2222);
+		ed.setNome("Ed");
+		ed.setSalario(2000);
 		
-		boolean aut = nico.autentica(2222);
+		ge.setNome("Geraldo");
+		ge.setSalario(1000);
 		
-		System.out.println(nico.getDados());
-		System.out.println(nico.getBonificacao());
+		nico.setNome("Nico");
+		nico.setSalario(1500);
+		
+		ControleBonificacao ctrl = new ControleBonificacao();
+		ctrl.registra(nico);
+		ctrl.registra(ge);
+		ctrl.registra(ed);
+		
+		System.out.println(ctrl.getSoma());
 	}
 
 }
